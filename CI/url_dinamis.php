31f51516984404b8f6706e97b182b7b901575ef8
zeroturn <fahruddinakas@gmail.com>
2019-10-04 00:25:06 +0700

    Update and rename url dinamis.php to url_dinamis.php

--- CI/url_dinamis.php
+++ CI/url_dinamis.php
@@ -1,3 +1,7 @@
+Untuk mengatur konfigurasi base_url agar menjadi dinamis, silakan ganti konfigurasi default CodeIgniter:
+$config['base_url'] = '';
+
+Dengan konfigurasi di bawah ini:
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
